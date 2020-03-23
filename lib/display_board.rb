d7ff22a1bce722ed335f068dc5902f4329233fa7
Jeanette Gonzalez <jeanettegonzalezlovesu@gmail.com>
2020-03-23 17:02:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -130,7 +130,6 @@ end
 
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
-  board[4] = "X"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
